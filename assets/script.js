3d6f5d6c89fabe23e23e476a8d13d3de2dfa1372
Sheetal Mangate <sheetal.mangate@gmail.com>
2024-06-26 06:53:38 -0700

    adding js file changes

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+/* const topic = "HTML";
+if( topic === "HTML" ) {
+
+    console.log("Lets Study HTML!");
+
+} else if( topic === "CSS" ) {
+    console.log("Lets Study CSS!");
+} else if( topic === "JavaScript" ) {
+    console.log("Lets Study JavaScript!");
+} else if( topic === "Git" ) {
+    console.log("Lets Study Git!");
+} else {
+    console.log("Please try again");
+} */
+
+const topics = [ "HTML", "CSS", "Git", "JavaScript"];
+
+for( let x =0; x < topics.length; x++) {
+
+     console.log(topics[x]);
+}
